4f98580fc3429d5f7bcd99b3a51a397d90d2360c
Vladimir <valdemar.leontev@gmail.com>
2022-06-12 18:14:09 +0300

    Bug fix

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { useLoader } from '@react-three/fiber';
-import { TextureLoader } from 'three/src/loaders/TextureLoader';
-import texture from '../images/TexturesCom_Leather0113_2_seamless_S.jpg'
+// import { useLoader } from '@react-three/fiber';
+// import { TextureLoader } from 'three/src/loaders/TextureLoader';
+// import texture from '../images/TexturesCom_Leather0113_2_seamless_S.jpg'
 
 export default function Box() {
 // const colorMap = useLoader(TextureLoader, texture);
