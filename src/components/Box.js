3db5cf5b8d109a6ba2178bc23b77cedded8a2e16
Vladimir <valdemar.leontev@gmail.com>
2022-06-12 18:12:43 +0300

    Bug fix

--- src/components/Box.js
+++ src/components/Box.js
@@ -4,11 +4,11 @@ import { TextureLoader } from 'three/src/loaders/TextureLoader';
 import texture from '../images/TexturesCom_Leather0113_2_seamless_S.jpg'
 
 export default function Box() {
-const colorMap = useLoader(TextureLoader, texture);
+// const colorMap = useLoader(TextureLoader, texture);
 
     return <mesh rotation={[90, 0, 20]}>
         <boxBufferGeometry attach='geometry' args={[3, 3, 3]} />
-
-        <meshStandardMaterial map={colorMap} />
+{/* 
+        <meshStandardMaterial map={colorMap} /> */}
     </mesh>;
 }
