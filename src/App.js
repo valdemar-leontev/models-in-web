9c5754069ba736b011f4868e6e6accf025d55bd0
valdemar-leontev <valdemar.leontev@gmail.com>
2023-06-07 16:59:01 +0300

    add contents of readme file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import './App.css'
-// import Face from './components/Face'
-// import Sphere from './components/AnimatedSphere'
+import Face from './components/Face'
+import Sphere from './components/AnimatedSphere'
 import Box from './components/Box'
 import styled from "styled-components";
 import { Canvas } from "@react-three/fiber";
@@ -10,33 +10,16 @@ import { OrbitControls } from '@react-three/drei';
 export default function App() {
   return (
     <Wrapper className="App">
-       <Canvas className="canvas">
-        <OrbitControls autoRotate={true} autoRotateSpeed={1} enableZoom={false} />
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[-2, 5, 2]} intensity={1} />
-        <Suspense fallback={() => <h1>Hello</h1>}>
-          <Box />
-        </Suspense>
-      </Canvas>
-
-      {/* <Canvas className="canvas">
+      <Canvas className="canvas">
         <OrbitControls autoRotate={true} autoRotateSpeed={1} enableZoom={false} />
         <ambientLight intensity={0.5} />
         <directionalLight position={[-2, 5, 2]} intensity={1} />
         <Suspense fallback={'Loading...'}>
           <Sphere />
+          {/* <Box />
+          <Face /> */}
         </Suspense>
-      </Canvas> */}
-
-      {/* <Canvas className="canvas">
-        <OrbitControls autoRotate={false} autoRotateSpeed={0} enableZoom={false} />
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[-2, 5, 2]} intensity={1} />
-        <Suspense fallback={null}>
-          <Face />
-        </Suspense>
-      </Canvas>   */}
-    
+      </Canvas>
     </Wrapper>
   );
 }
